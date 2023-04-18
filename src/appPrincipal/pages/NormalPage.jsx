04225be6340250cce4c8994896a8680121611fc2
mbernallaura <mbernallaura@gmail.com>
2023-04-17 22:12:04 -0500

    Parte visual de la pagina de normal page

--- src/appPrincipal/pages/NormalPage.jsx
+++ src/appPrincipal/pages/NormalPage.jsx
@@ -1,7 +1,28 @@
+import { Navbar } from "../../ui/components/Navbar"
 
 
 export const NormalPage = () => {
+  const usuario = JSON.parse(localStorage.getItem('user'));
+
   return (
-    <h5>NormalPage</h5>
+    <div className="pages">
+      <Navbar user={ usuario }/>
+      <div className="normalPage">
+        <div>
+          <img src="../src/assets/logoPanzofi.png" alt="logo" className="logoNormal"/>
+        </div>
+        <div>
+          <h1 className="card-title"> <b>Panzofi</b> </h1>
+          <p className="mt-4">
+            Usted como usuario podra oprimir cualquiera de los botones a su elección, recuerde que esto es para
+            hacer un estudio y tomar datos de cuantas veces puede oprimimr uno u otro. 
+          </p>
+          <div className="mt-10 buttons">
+            <button className="btn btn-danger">Boton 1</button>
+            <button className="btn btn-dark">Boton 2</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
